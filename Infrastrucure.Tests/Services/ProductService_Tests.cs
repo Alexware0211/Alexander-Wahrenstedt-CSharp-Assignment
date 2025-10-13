25cf00f9136d30bca0a5afc2118289f9c9fd31ea
Alexander <alexander.warenstedt@gmail.com>
2025-10-13 11:27:45 +0200

    Added a test for if product were added to list
    Also had to update Infrastrucutre.csproj due to accidently making it as a console app and not a class library
    
    Also had to update infrastructure.tests name as I had called it "Infrastrucure"

--- Infrastrucure.Tests/Services/ProductService_Tests.cs
+++ Infrastrucure.Tests/Services/ProductService_Tests.cs
@@ -1,7 +1,34 @@
-﻿using Moq;
+﻿
+using Infrastructure.Services;
+using Infrastructure.Interfaces;
+using Infrastructure.Models;
+using Moq;
 
 namespace Infrastrucure.Tests.Services;
 
-internal class ProductService_Tests
+public class ProductService_Tests
 {
+    private readonly Mock<IFileService> _fileServiceMock;
+    private readonly ProductService _productService;
+
+    public ProductService_Tests()
+    {
+        _fileServiceMock = new Mock<IFileService>();
+        _productService = new ProductService(_fileServiceMock.Object, @"c:\");
+    }
+
+    [Fact]
+    public void AddProductToList_ShouldReturnTrue_WhenProductAddedToList()
+    {
+        //Arrange
+        _fileServiceMock.Setup(fs => fs
+            .SaveContentToFile(It.IsAny<string>(), It.IsAny<string>()))
+            .Returns(new FileResult { Success = true });
+
+        //Act
+        var result = _productService.AddProductToList(new Product { Name = "Test", Price = 10 });
+
+        //Assert
+        Assert.True(result.Success);
+    }
 }
