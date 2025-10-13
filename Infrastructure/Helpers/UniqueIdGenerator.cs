886f2d5f2af01d8a99f125c4aeec4253b973b019
Alexander <alexander.warenstedt@gmail.com>
2025-10-13 09:46:15 +0200

    UniqueIDGenerator Helper & Interface created

---
+++ Infrastructure/Helpers/UniqueIdGenerator.cs
@@ -0,0 +1,11 @@
+﻿using Infrastructure.Interfaces;
+
+namespace Infrastructure.Helpers;
+
+public class UniqueIdGenerator : IUniqueIdGenerator
+{
+    public static string GenerateID()
+    {
+        return Guid.NewGuid().ToString();
+    }
+}
